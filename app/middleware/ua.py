1c29fdb3c53b957e4eedfc6630f2ed7ee880fb0b
stpnvkirill <stpnv.kirill.o@gmail.com>
2026-02-17 13:26:15 +0300

    🕵️‍♀️ Add UserAgentMiddleware

---
+++ app/middleware/ua.py
@@ -0,0 +1,17 @@
+from flask import g, request
+from user_agents import parse
+
+from .base import BaseMiddlaware
+
+
+class UserAgentMiddleware(BaseMiddlaware):
+    def before(self):
+        if request.headers.getlist("X-Forwarded-For"):
+            ip_address = request.headers.getlist("X-Forwarded-For")[0]
+        else:
+            ip_address = request.remote_addr
+
+        ua_string = request.headers.get("User-Agent", "Unknown")
+
+        g.ip_address = ip_address
+        g.user_agent = parse(ua_string)
