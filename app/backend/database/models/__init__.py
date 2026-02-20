871919838e940e57e89f7503d2cfa195ae3eb9da
stpnvkirill <stpnv.kirill.o@gmail.com>
2026-02-20 10:50:18 +0300

    🔒 Permission models

--- app/backend/database/models/__init__.py
+++ app/backend/database/models/__init__.py
@@ -1,3 +1,10 @@
+from .permissions import (
+    Permission,
+    PermissionGroup,
+    permission_group_permissions,
+    user_permission_groups,
+    user_permissions,
+)
 from .sessions import OS, Browser, UserSession
 from .shared import Base
 from .users import User
